947feb20956dec79f2ffd5a31f5fe0ce3f1a92fd
BertoDiaz <heriberto.diazluis@gmail.com>
2018-09-05 15:08:21 +0200

    Fixed somre error in custom buttons.

--- lib/RectangleButton.py
+++ lib/RectangleButton.py
@@ -1,6 +1,23 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+"""
+Copyright (C) 2018  Heriberto J. Díaz Luis-Ravelo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from PyQt5.QtCore import QPointF, pyqtProperty
+from PyQt5.QtGui import QColor, QPainter, QPen, QLinearGradient, QBrush, QPolygonF
+from PyQt5.QtWidgets import QAbstractButton
 
 
 class RectangleButton(QAbstractButton):
